d0cc3b852dc83bddb6499fc96a712da7ef8fa913
dq-aem-anilkumar <anilkumarg@digiquadsolutions.com>
2025-05-08 12:36:32 +0530

    done with Windows Page CopilotComponent

---
+++ core/src/main/java/com/aem/trg/microsoft/core/models/CopilotComponent.java
@@ -0,0 +1,88 @@
+package com.aem.trg.microsoft.core.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.wcm.core.components.models.Image;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+@Model(adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class CopilotComponent {
+
+
+    @ValueMapValue
+    private String fileReference;
+
+    @ValueMapValue
+    private String text;
+
+    @ChildResource(name = "link")
+    private Resource linkResource;
+
+    private String link;
+    @Inject
+    private String buttonText;
+
+    @Inject
+    private String buttonLink;
+
+    @Inject
+    private String title;
+    @ValueMapValue
+    private String description2;
+
+    @ValueMapValue
+    private boolean showButton;
+
+    public boolean isShowButton() {
+        return showButton;
+    }
+
+    @PostConstruct
+    protected void init() {
+        if (linkResource != null) {
+            link = linkResource.getValueMap().get("href", String.class);
+        }
+    }
+
+    public String getFileReference() {
+        return fileReference;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getButtonText() {
+        return buttonText;
+    }
+
+    public String getButtonLink() {
+        if (buttonLink != null && !buttonLink.startsWith("http")) {
+            return "https://" + buttonLink;
+        }
+        return buttonLink;
+    }
+
+
+
+
+    public String getDescription2() {
+        return description2;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+}

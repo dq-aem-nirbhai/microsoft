fa3381d1f976c996675df138cac851978b6c704d
dq-aem-nirbhai <nirbhaip@digiquadsolutions.com>
2025-05-10 11:53:24 +0530

    updated few changes

--- core/src/main/java/com/aem/trg/microsoft/core/models/ReusableComponent.java
+++ core/src/main/java/com/aem/trg/microsoft/core/models/ReusableComponent.java
@@ -1,15 +1,13 @@
 package com.aem.trg.microsoft.core.models;
 
-import com.adobe.cq.export.json.ComponentExporter;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 
+import javax.inject.Inject;
 
 
 
@@ -83,7 +81,6 @@ public class ReusableComponent {
         }
         return buttonLink;
     }
-
 
 
 

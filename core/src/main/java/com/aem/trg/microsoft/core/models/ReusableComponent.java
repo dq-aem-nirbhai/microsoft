8dddbd5cb058bda9b54ee9cc8956ea27daac6fd0
koratlasaraswathi <saraswathik@digiquadsolutions.com>
2025-05-12 18:10:19 +0530

    updated planning card

--- core/src/main/java/com/aem/trg/microsoft/core/models/ReusableComponent.java
+++ core/src/main/java/com/aem/trg/microsoft/core/models/ReusableComponent.java
@@ -1,9 +1,11 @@
 package com.aem.trg.microsoft.core.models;
 
+
 import com.adobe.cq.export.json.ComponentExporter;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 

629c271f8aba9aefb107a03afe4b2bf05e28b2cb
koratlasaraswathi <saraswathik@digiquadsolutions.com>
2025-05-13 12:03:15 +0530

    updated Header component

---
+++ core/src/main/java/com/aem/trg/microsoft/core/models/HeaderDropDown.java
@@ -0,0 +1,55 @@
+package com.aem.trg.microsoft.core.models;
+
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import java.util.List;
+
+@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class HeaderDropDown {
+    @ChildResource
+    private List<DropDownLinkItems> links;
+    @ChildResource
+    private List<SubItems>items;
+    @ValueMapValue
+    private String sitemap;
+    @ValueMapValue
+    private String sitemaplink;
+
+    public String getSitemap() {
+        return sitemap;
+    }
+
+    public String getSitemaplink() {
+        return sitemaplink;
+    }
+
+    public List<DropDownLinkItems> getLinks() {
+        return links;
+    }
+
+    public List<SubItems> getItems() {
+        return items;
+    }
+
+    @Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+    public static class SubItems{
+        @ValueMapValue
+        private String title;
+        @ChildResource
+        private List<DropDownLinkItems> listoflinks1;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public List<DropDownLinkItems> getListoflinks1() {
+            return listoflinks1;
+        }
+    }
+
+}
